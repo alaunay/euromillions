68d688459ddc38c08786c3e4691dcebea35e2367
Arnaud Launay <alaunay@cusae.com>
2015-10-25 00:04:16 +0200

    Get 5 random numbers and 2 stars

---
+++ euromillions.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+echo "5 numéros:"
+echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
+echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
+echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
+echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
+echo "$[ ( $RANDOM % 50 + 1 ) ] "
+
+echo "2 étoiles:"
+echo -n "$[ ( $RANDOM % 11 + 1 ) ] "
+echo "$[ ( $RANDOM % 11 + 1 ) ] "

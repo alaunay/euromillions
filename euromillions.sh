994bf168b513fa661791f56f05a4cfe0c0887693
Arnaud Launay <asl@launay.org>
2015-10-27 17:50:55 +0100

    Use functions

--- euromillions.sh
+++ euromillions.sh
@@ -1,12 +1,22 @@
 #! /bin/sh
 
-echo "5 numéros:"
-echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
-echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
-echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
-echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
-echo "$[ ( $RANDOM % 50 + 1 ) ] "
+# Get and print numbers
+numbers () {
+	echo "5 numéros:"
+	echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
+	echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
+	echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
+	echo -n "$[ ( $RANDOM % 50 + 1 ) ] "
+	echo "$[ ( $RANDOM % 50 + 1 ) ] "
+}
 
-echo "2 étoiles:"
-echo -n "$[ ( $RANDOM % 11 + 1 ) ] "
-echo "$[ ( $RANDOM % 11 + 1 ) ] "
+# Get and print stars
+stars () {
+	echo "2 étoiles:"
+	echo -n "$[ ( $RANDOM % 11 + 1 ) ] "
+	echo "$[ ( $RANDOM % 11 + 1 ) ] "
+}
+
+# Start functions
+numbers
+stars

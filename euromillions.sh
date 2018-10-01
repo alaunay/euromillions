801e3b8122b92176754bedab975169bb4580cc57
Arnaud Launay <alaunay@cusae.com>
2018-10-01 11:16:11 +0200

    12 stars in 2016

--- euromillions.sh
+++ euromillions.sh
@@ -13,12 +13,12 @@ numbers () {
 # Get and print stars
 stars () {
 	# initialize identical stars
-	stars[0]=$[ ( $RANDOM % 11 + 1 ) ]
+	stars[0]=$[ ( $RANDOM % 12 + 1 ) ]
 	stars[1]=${stars[0]}
 
 	# loop until stars are different
 	while [ ${stars[0]} = ${stars[1]} ]; do
-		stars[1]="$[ ( $RANDOM % 11 + 1 ) ] "
+		stars[1]="$[ ( $RANDOM % 12 + 1 ) ] "
 	done
 
 	# Sort stars
